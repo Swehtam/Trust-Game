d4c6d03d493dbd0dbb0143bea4b89c73abbd84bd
Swehtam <mathewsalves1@outlook.com>
2019-11-03 22:07:23 -0300

    first commit

---
+++ Assets/GameControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public PlayerControl player1;
+    public PlayerControl player2;
+    public Text roundLog;
+    private string roundText;
+    private bool player1Decision;
+
+    // Update is called once per frame
+    void Update()
+    {
+        roundLog.text = roundText;
+    }
+
+    //if action is:
+    //true - cooperate
+    //false - cheat
+    //-------------------
+    //if player is:
+    //true - player1
+    //false - player2
+    public void RoundDecision(bool action, bool player)
+    {
+
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    private int score;
+    public Text playerScore;
+    public GameControl gameControl;
+
+    void Update()
+    {
+        playerScore.text = score.ToString();
+    }
+
+    public void Cooperate()
+    {
+
+    }
+
+    public void Cheat()
+    {
+
+    }
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+    }
+}
