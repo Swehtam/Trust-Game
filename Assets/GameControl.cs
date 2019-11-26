f3d5d3310b4670f9886ec34801be19bb8c3effc0
Swehtam <mathewsalves1@outlook.com>
2019-11-26 18:15:05 -0300

    all functionalities are implemented, but it doesn't have any AI

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -2,30 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GameControl : MonoBehaviour
 {
     public PlayerControl player1;
     public PlayerControl player2;
     public Text roundLog;
+    public Text endText;
+
     private string roundText;
-    private bool player1Decision;
+    private int maxRounds;
+    private PlayerControl.PlayerDecision playerD1;
+    private PlayerControl.PlayerDecision playerD2;
+
+    private void Start()
+    {
+        System.Random random = new System.Random();
+        maxRounds = random.Next(5, 11);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        roundLog.text = roundText;
+        if(maxRounds != 0)
+        {
+            CheckForDecision();
+        }
+        else
+        {
+            endText.text = "Game over";
+            StartCoroutine(player1.DisableButtons());
+            StartCoroutine(player2.DisableButtons());
+        }
+        
+    }
+    
+    public void InstancePlayerD(PlayerControl.PlayerDecision playerD)
+    {
+        if (playerD.PlayerNumb == 1)
+        {
+            playerD1 = playerD;
+            
+        }
+        else
+        {
+            playerD2 = playerD;
+        }
     }
 
-    //if action is:
-    //true - cooperate
-    //false - cheat
-    //-------------------
-    //if player is:
-    //true - player1
-    //false - player2
-    public void RoundDecision(bool action, bool player)
+    private void CheckForDecision()
     {
+        if (playerD1.RoundDecision && playerD2.RoundDecision) //Both of them made a decision
+        {
+            int scoreP1 = 0;
+            int scoreP2 = 0;
 
+            roundLog.text = "Player 1: ";
+            if (playerD1.Decision) //Cooperated
+            {
+                roundLog.text += "Cooperated";
+                scoreP1 -= 1;
+                scoreP2 += 3;
+            }
+            else //Cheated
+            {
+                roundLog.text += "Cheated";
+            }
+
+            roundLog.text += "\nPlayer 2: ";
+            if (playerD2.Decision) //Cooperated
+            {
+                roundLog.text += "Cooperated";
+                scoreP1 += 3;
+                scoreP2 -= 1;
+            }
+            else //Cheated
+            {
+                roundLog.text += "Cheated";
+            }
+
+            roundLog.text += "\n\n" + scoreP1 + "/" + scoreP2;
+
+            playerD1.RoundDecision = false;
+            playerD2.RoundDecision = false;
+            StartCoroutine(UpdateScores(scoreP1, scoreP2));            
+        }
+    }
+
+    private IEnumerator UpdateScores(int scoreP1, int scoreP2)
+    {
+        yield return new WaitForSeconds(1);
+        player1.UpdateScore(scoreP1);
+        player2.UpdateScore(scoreP2);
+        
+        maxRounds -= 1;
     }
 }
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -5,9 +5,50 @@ using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
-    private int score;
+    public int playerNumb;
     public Text playerScore;
     public GameControl gameControl;
+    public Button cooperateButton;
+    public Button cheatButton;
+
+    private int score;
+    private PlayerDecision player;
+
+    public class PlayerDecision
+    {
+        private int number; //Player Number
+        private bool decision; //True if the player decided to cooperate
+                               //False if the player doesn't
+        private bool roundDecis; //True if the player already chose an option on this roud
+                                 //False if the player doesn't
+        public PlayerDecision(int num)
+        {
+            this.number = num;
+        }
+
+        public int PlayerNumb // property
+        {
+            get { return number; }
+        }
+
+        public bool Decision // property
+        {
+            get { return decision; }
+            set { decision = value; }
+        }
+        
+        public bool RoundDecision // property
+        {
+            get { return roundDecis; }
+            set { roundDecis = value; }
+        }
+    }
+
+    private void Awake()
+    {
+        player = new PlayerDecision(playerNumb);
+        gameControl.InstancePlayerD(player);
+    }
 
     void Update()
     {
@@ -16,16 +57,29 @@ public class PlayerControl : MonoBehaviour
 
     public void Cooperate()
     {
-
+        player.Decision = true;
+        player.RoundDecision = true;
+        StartCoroutine(DisableButtons());
     }
 
     public void Cheat()
     {
+        player.Decision = false;
+        player.RoundDecision = true;
+        StartCoroutine(DisableButtons());
+    }
 
+    public IEnumerator DisableButtons()
+    {
+        yield return new WaitForSeconds(0.05f);
+        cooperateButton.interactable = false;
+        cheatButton.interactable = false;
     }
 
     public void UpdateScore(int value)
     {
         score += value;
+        cooperateButton.interactable = true;
+        cheatButton.interactable = true;
     }
 }
